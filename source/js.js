2a41106e15c251dba64a6f3d4ca4377c260fc36b
chokoret <38235948+chokoret@users.noreply.github.com>
2020-11-10 14:31:23 -0800

    Add files via upload

---
+++ source/js.js
@@ -0,0 +1,8 @@
+function off() {
+  document.getElementById("overlay").style.opacity = "0";
+document.getElementById("overlay").style.visibility = "hidden";
+}
+function turn() {
+  document.getElementById("overlay2").style.opacity = "0.5";
+document.getElementById("overlay2").style.visibility = "visible";
+}
